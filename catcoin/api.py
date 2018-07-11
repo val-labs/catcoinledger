be6ec33a4767f9d4683669bb9f5073c40c1a1bcf
Joel Ward <jmward@gmail.com>
2018-07-10 21:04:12 -0700

    update

--- catcoin/api.py
+++ catcoin/api.py
@@ -1,6 +1,16 @@
 import os, sys, time, hashlib
 import peer2peer
 
+Ws = None
+
+def connect_network(addr='localhost:5454'):
+    global Ws
+    if addr.startswith(':'): addr = 'localhost' + addr
+    ws = peer2peer.conn( addr )
+    if not Ws:
+        Ws = ws
+    return ws
+
 def hashfile(filename):
     h = hashlib.new('ripemd160')
     with open(filename) as f:
@@ -17,7 +27,7 @@ def mkdir(path):
         
 def spawn(cmd, filename='pid', suffix=''):
     try:
-        suffix = " 1>stdout%s 2>stderr%s & echo $$ >" % (suffix,suffix)
+        suffix = " 1>logs/stdout%s 2>logs/stderr%s & echo $$ >" % (suffix,suffix)
         print os.system(cmd + suffix + filename)
         return int(open(filename).read())
     finally:
@@ -28,29 +38,20 @@ def system(cmd):
 
 def verify_block(filename, blkno, parent, hashstr):
     "verify block: does this guy's story check out?"
-    if not blkno:
-        raise "CANT SAVE A GENESIS BLOCK"
     return True
 
-def unsafe_store_block(filename, blkno, parent, hashstr):
-    #print "unsafe_store_block"+repr((filename, blkno, parent, hashstr))
+def store_block(filename, blkno, parent, hashstr):
+    if not verify_block(filename, blkno, parent, hashstr):
+        raise Exception("Bad Block")
     direc = 's/b/%s/%s' % (blkno, parent)
     try:    os.makedirs(direc)
     except: pass
     system('mv %s %s/%s' % (filename, direc, hashstr))
     return ('%s/%s' % (direc, hashstr))
 
-def store_block(filename, blkno, parent, hashstr):
-    #print "store_block"+repr((filename, blkno, parent, hashstr))
-    if not verify_block(filename, blkno, parent, hashstr):
-        raise Exception("Bad Block")
-    return unsafe_store_block(filename, blkno, parent, hashstr)
-
 def store_and_forward_block(filename, blkno, parent, hashstr):
-    #print "store_and_forward_block"+repr((filename, blkno, parent, hashstr))
     filename = store_block(filename, blkno, parent, hashstr)
-    #print "FORWARD ", filename
-    peer2peer.pub('127.0.0.1:5454', 'blocks', filename)
+    if Ws: peer2peer.sendv(['pub blocks', '2', open(filename).read()], Ws)
     return filename
 
 def sign_xtn(msg, keyfile, filename):
@@ -58,26 +59,46 @@ def sign_xtn(msg, keyfile, filename):
     with open(filename0,'w') as fw:  fw.write(msg)
     system('head -1<%s|cat - %s|pkcrypt sign %s -h >%s'
            % (keyfile,filename0,keyfile,filename))
+    system('rm ' + filename0)
+
+def mine_block(blkno, parent_hashstr, pattern, difficulty_prefix='0'):
+    system('echo "- - BID: b/%s/%s" >msgx' % (blkno, parent_hashstr))
+    system('echo "  - Date:" `date -u +%Y-%M-%dT%H:%M:%SZ` >>msgx')
+    system('cat %s >>msgx' % pattern)
+    h = hashlib.new('ripemd160')
+    with open('msgx') as f:
+        for line in f.readlines():
+            h.update( line )
+    n = 0; hashstr = 'x'
+    while not hashstr.startswith(difficulty_prefix):
+        n += 1; h2 = h.copy(); r = '- Nonce: %x\n' % n
+        h2.update(r); hashstr = h2.hexdigest()
+    with open('msgx','a') as fw:
+        fw.write(r)
+    store_and_forward_block('msgx', blkno, parent_hashstr, hashstr)
+    system('rm '+pattern)
+    return blkno+1, hashstr
 
 def init_chain(name, data=None):
     mkdir(name)
     os.chdir(name)
+    mkdir('logs')
     mkdir('wallets')
     mkdir('s/b')
     blkno = 0
     parent_hashstr = '0'*40
     inp_name = 'genesis.txt'
-    with open(inp_name,'w') as fw:
+    system('mv ../id.root wallets')
+    system('head -1 <wallets/id.root >genesis.txt')
+    with open(inp_name,'a') as fw:
         fw.write(data or """\
 - Genesis: |
     Mee-OW!
     I am cat!
     Hear me roar!
 """)
-    hashstr = hashfile(inp_name)
-    # do this otherwise saving the genesis block will be an error
-    unsafe_store_block(inp_name, 0, parent_hashstr, hashstr)
-    return 1, hashstr
+    ret = mine_block(0,parent_hashstr,inp_name)
+    return ret
 
 def start_chain():
     print "Starting WebServer..."
@@ -88,4 +109,5 @@ def start_chain():
     pid2 = spawn('PYTHONPATH=.. python '+
                  '../peer2peer/peer2peer.py serve --port 5454', 'pid2', '.p2p')
     print "server pid =", pid2
+    time.sleep(0.2)
     return [pid, pid2]
--- catcoin/test.py
+++ catcoin/test.py
@@ -4,94 +4,79 @@
 import os, sys, time, hashlib
 from catcoin.api import *
 
-def mine_block(blkno, parent_hashstr, pattern):
-    system('echo "- - BID: b/%s/%s" >msgx' % (blkno, parent_hashstr))
-    system('echo "  - Date:" `date +%Y-%M-%dT%H:%M:%S%z` >>msgx')
-    system('cat %s >>msgx' % pattern)
-    import random
-    h = hashlib.new('ripemd160')
-    with open('msgx') as f:
-        for line in f.readlines():
-            h.update( line )
-    hashstr = 'x'
-    n = 0
-    while not hashstr.startswith('00000'):
-        h2 = h.copy()
-        #r = '- Nonce: ' + str(random.random())
-        r = '- Nonce: %x\n' % n
-        n += 1
-        h2.update(r)
-        hashstr = h2.hexdigest()
-        #time.sleep(0.01)
-        pass
-    print hashstr
-    with open('msgx','a') as fw:
-        fw.write(r)
-    store_and_forward_block('msgx', blkno, parent_hashstr, hashstr)
-    return blkno+1, hashstr
-
-def sign_and_send_xtn(msg,kfile,blkno,parent_hashstr):
+def sign_and_send_xtn(msg,kfile,blkno,hashstr):
     sign_xtn(msg,kfile,'msg1')
-    return mine_block(blkno, parent_hashstr,'msg1')
+    return mine_block(blkno, hashstr,'msg1')
+
+def create_wallet(nick,dir='wallets/'):
+    system('pkcrypt genpair >%sid.%s' % (dir,nick))
+    system('chmod 600 %sid.%s' % (dir,nick))
+    return '%sid.%s' % (dir,nick)
 
 def test():
-    blkno, parent_hashstr = init_chain('test.data')
+    # let's create the root identity
+    root = create_wallet('root','')
+
+    blkno, hashstr = init_chain('test.data')
+
     start_chain()
 
+    connect_network()
+    
     # let's create a new identity (or two)
-    system('pkcrypt genpair >id.1')
+    id1 = create_wallet('1')
 
-    blkno, parent_hashstr = sign_and_send_xtn("""\
+    blkno, hashstr = sign_and_send_xtn("""\
   - Name:= Fluffy
   - Meow: Meow?  Is anyone out there?
-""",'id.1',blkno,parent_hashstr)
+""",id1,blkno,hashstr)
 
-    system('pkcrypt genpair >id.2')
+    id2 = create_wallet('2')
 
-    blkno, parent_hashstr = sign_and_send_xtn("""\
+    blkno, hashstr = sign_and_send_xtn("""\
   - Name:= Patches
   - Purr: I am out here, @Fluffy!
-""",'id.2',blkno,parent_hashstr)
+""",id2,blkno,hashstr)
 
-    blkno, parent_hashstr = sign_and_send_xtn("""\
+    blkno, hashstr = sign_and_send_xtn("""\
   - Meow: Tell me more, @Patches!
-""",'id.1',blkno,parent_hashstr)
+""",id1,blkno,hashstr)
 
-    blkno, parent_hashstr = sign_and_send_xtn("""\
+    blkno, hashstr = sign_and_send_xtn("""\
   - Hiss: Who wants to know??
-""",'id.2',blkno,parent_hashstr)
+""",id2,blkno,hashstr)
 
-    blkno, parent_hashstr = sign_and_send_xtn("""\
+    blkno, hashstr = sign_and_send_xtn("""\
   - Hiss: Who wants to know2??
-""",'id.2',blkno,parent_hashstr)
+""",id2,blkno,hashstr)
 
-    blkno, parent_hashstr = sign_and_send_xtn("""\
+    blkno, hashstr = sign_and_send_xtn("""\
   - Hiss: Who wants to know3??
-""",'id.2',blkno,parent_hashstr)
+""",id2,blkno,hashstr)
 
-    blkno, parent_hashstr = sign_and_send_xtn("""\
+    blkno, hashstr = sign_and_send_xtn("""\
   - Hiss: Who wants to know4??
-""",'id.2',blkno,parent_hashstr)
+""",id2,blkno,hashstr)
 
-    blkno, parent_hashstr = sign_and_send_xtn("""\
+    blkno, hashstr = sign_and_send_xtn("""\
   - Hiss: Who wants to know5??
-""",'id.2',blkno,parent_hashstr)
+""",id2,blkno,hashstr)
 
-    blkno, parent_hashstr = sign_and_send_xtn("""\
+    blkno, hashstr = sign_and_send_xtn("""\
   - Hiss: Who wants to know6??
-""",'id.2',blkno,parent_hashstr)
+""",id2,blkno,hashstr)
 
-    blkno, parent_hashstr = sign_and_send_xtn("""\
+    blkno, hashstr = sign_and_send_xtn("""\
   - Hiss: Who wants to know7??
-""",'id.2',blkno,parent_hashstr)
+""",id2,blkno,hashstr)
 
-    blkno, parent_hashstr = sign_and_send_xtn("""\
+    blkno, hashstr = sign_and_send_xtn("""\
   - Hiss: Who wants to know8??
-""",'id.2',blkno,parent_hashstr)
+""",id2,blkno,hashstr)
 
-    blkno, parent_hashstr = sign_and_send_xtn("""\
+    blkno, hashstr = sign_and_send_xtn("""\
   - Hiss: Who wants to know9??
-""",'id.2',blkno,parent_hashstr)
+""",id2,blkno,hashstr)
 
     system('tree -sa')
     #time.sleep(600000)
